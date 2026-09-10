47a34fbef1d480c17504c6974e6a2f8d593778c5
Jesaias <jesaias.fernandes@academico.ifpb.edu.br>
2026-09-10 07:44:18 -0300

    Controller Métodos

--- ifgram/src/main/java/ifgram/Controller/UsersController.java
+++ ifgram/src/main/java/ifgram/Controller/UsersController.java
@@ -1,8 +1,6 @@
 package ifgram.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
@@ -11,6 +9,22 @@ public class UsersController {
     @GetMapping
     public String getUser(){
         return "OLá";
+    }
+    @PostMapping
+    public String postUser(){
+        return "chamei o endpoint como um POST";
+    }
+    @DeleteMapping
+    public String deleteUser(){
+        return "chamei o endpoint como um DELETE";
+    }
+    @PutMapping
+    public String putUser(){
+        return "chamei o endpoint como um PUT";
+    }
+    @PatchMapping
+    public String patchUser(){
+        return "chamei o endpoint como um Patch";
     }
 
 }
